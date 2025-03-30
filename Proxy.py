330cb9c94927a462629b329870bedc4aa6518999
gengchao-yang <a1899237@adelaide.edu.au>
2025-03-30 22:25:13 +1030

    Forwarded origin server response to client.

--- Proxy.py
+++ Proxy.py
@@ -223,6 +223,10 @@ while True:
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+
+      # Using sendall() to Send the full response (bytes) to the client.
+      clientSocket.sendall(response)
+
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
